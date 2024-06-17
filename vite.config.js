4d8fcb053f5de7396c948c7a3839e55d3eb90e56
Berk <139360217+Beko2023@users.noreply.github.com>
2024-06-17 21:10:48 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import he from "he";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      external: [he],
+      external: ["he.js"],
     },
   },
 });
